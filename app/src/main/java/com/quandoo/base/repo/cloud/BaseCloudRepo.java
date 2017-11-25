3eae9e9360011cc05935da151488f73ff268f517
Eslam Hussein <eslam.hussein.awad@gmail.com>
2017-11-25 19:52:18 +0200

    remove Rx Adapter for retrofit

--- app/src/main/java/com/quandoo/base/repo/cloud/BaseCloudRepo.java
+++ app/src/main/java/com/quandoo/base/repo/cloud/BaseCloudRepo.java
@@ -1,18 +1,11 @@
 package com.quandoo.base.repo.cloud;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
 
-import io.realm.RealmObject;
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BaseCloudRepo {
@@ -24,31 +17,15 @@ public class BaseCloudRepo {
 
     protected <T> T execute(Call<T> call) throws IOException {
         Response<T> response = call.execute();
-//        if (!response.isSuccessful()) {
-//            throw response.errorBody().er  // TODO: HTTP failure exception
-//        }
         return response.body();
     }
+
     private Retrofit retrofit() {
         OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
                 .build();
-
-       /* Gson gson = new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
-            @Override
-            public boolean shouldSkipField(FieldAttributes f) {
-                return f.getDeclaringClass().equals(RealmObject.class);
-            }
-
-            @Override
-            public boolean shouldSkipClass(Class<?> clazz) {
-                return false;
-            }
-        }).create();*/
-
         return new Retrofit.Builder()
                 .baseUrl(CloudConfig.BASE_URL).client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
